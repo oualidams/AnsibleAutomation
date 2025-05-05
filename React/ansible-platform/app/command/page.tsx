fc2df5a8ed7f9c9a16b2aa639064d18c9869d1a0
Oualid Amsaguine <oamsaguine@hps-worldwide.com>
2025-05-05 16:10:49 +0100

    resoliving an error1

--- React/ansible-platform/app/command/page.tsx
+++ React/ansible-platform/app/command/page.tsx
@@ -1,6 +1,7 @@
+"use client";
 import dynamic from "next/dynamic";
 
-const TerminalClient = dynamic(() => import("../../components/terminal-client"), { ssr: false });
+const TerminalClient = dynamic(() => import("@/components/terminal-client"), { ssr: false });
 
 export default function TerminalPage() {
   return <TerminalClient />;
